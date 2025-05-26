6160ff48aeab461586006d191912d0705b200351
Brendan Dileo <dileob23@gmail.com>
2025-05-26 19:36:44 -0400

    Add GET and POST routes

--- src/routes/comicRoutes.js
+++ src/routes/comicRoutes.js
@@ -16,25 +16,44 @@ router.get('/', async (req, res) => {
     }
 });
 
-// GET single comic
-router.get('/:id', (req, res) => {
+// GET single comic by id
+router.get('/:id', async (req, res) => {
+    try {
+        const comic = await Comic.findById(req.params.id);
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 });
 
 // Create a new comic entry
 router.post('/', (req, res) => {
+    const newComic = new Comic(req.body);
+
+    try {
+        const savedComic = await.newComic.save();
+        res.status(201).json(savedComic);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 });
 
 // Update a comic
 router.put('/:id', (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 });
 
 // Delete a comic
 router.delete('/:id', (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 });
 
 module.exports = router;
-
-
-
-
-
