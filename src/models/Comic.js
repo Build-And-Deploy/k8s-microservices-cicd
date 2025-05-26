3fb587c8d80e15599410574abe6c457b724ee398
Brendan Dileo <dileob23@gmail.com>
2025-05-26 18:33:09 -0400

    Add table fields

--- src/models/Comic.js
+++ src/models/Comic.js
@@ -5,4 +5,12 @@ const mongoose = require('mongoose');
 
 const ComicSchema = new mongoose.Schema({
     title: {type: String, required: true },
-})
+    author: String,
+    genre: String,
+    volume: Number,
+    issue: Number,
+    publishedDate: Date,
+    summary: String,
+}, { timestamps: true });
+
+module.exports = mongoose.model('Comic', ComicSchema);
