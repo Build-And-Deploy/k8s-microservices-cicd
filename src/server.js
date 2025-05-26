83b26115921a32f8a26170b5ca92cbf2f173167a
Brendan Dileo <dileob23@gmail.com>
2025-05-26 18:00:45 -0400

    Setup basic node server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,14 @@
 // server.js
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 3001;
+
+app.use(cors());      
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
