990072f624772c6c8b7de05bd2d1b961cc91b58f
Brendan Dileo <dileob23@gmail.com>
2025-05-26 18:28:32 -0400

    Update db

--- src/db.js
+++ src/db.js
@@ -5,9 +5,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-
+        const conn = await mongoose.connect('', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
     } catch (err) {
         console.error('', err);
         process.exit(1);
     }
-}
+};
+
+module.exports = connectDB;
